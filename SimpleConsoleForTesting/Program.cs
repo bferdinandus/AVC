c0278cffde3a895b3a45a589adc315fd5f23be6f
Ben Ferdinandus <2flyfish@gmail.com>
2021-06-13 10:59:01 +0200

    experiment :-(

--- SimpleConsoleForTesting/Program.cs
+++ SimpleConsoleForTesting/Program.cs
@@ -1,22 +1,10 @@
-﻿using System;
-using CoreAudio;
-
-namespace SimpleConsoleForTesting
+﻿namespace SimpleConsoleForTesting
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MMDeviceEnumerator deviceEnumerator = new MMDeviceEnumerator();
-            MMDevice device = deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
-            SessionCollection sessions = device.AudioSessionManager2.Sessions;
-            foreach (AudioSessionControl2 session in sessions)
-            {
-                Console.WriteLine($"Session Identifier: {session.GetSessionIdentifier}");
-                Console.WriteLine($"Session Name: {session.DisplayName}");
-                Console.WriteLine($"Session IconPath: {session.IconPath}");
-                Console.WriteLine("===================");
-            }
+
         }
     }
 }
