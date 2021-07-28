38108b7b7854b65cdc109b58b626ecad93710df5
Ben Ferdinandus <2flyfish@gmail.com>
2021-07-28 12:25:02 +0200

    start of serial communication

--- AVC.Core/App.cs
+++ AVC.Core/App.cs
@@ -19,6 +19,8 @@ namespace AVC.Core
 
             Mvx.IoCProvider.LazyConstructAndRegisterSingleton<IAudioService, AudioService>();
 
+            Mvx.IoCProvider.ConstructAndRegisterSingleton<ISerialCommunication, SerialCommunication>();
+
             RegisterAppStart<VolumeSliderViewModel>();
         }
     }
